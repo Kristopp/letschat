dc699592893add775357b7eb712a870f5fc2cf75
Kristo Palu <44571530+Kristopp@users.noreply.github.com>
2021-06-09 20:59:36 +0300

    basic styling on login page

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,47 @@
+import { Button } from "@material-ui/core";
 import Head from "next/head";
 import styled from "styled-components";
 
 function Login() {
+
+    
+
   return (
     <Container>
       <Head>
         <title>Login</title>
       </Head>
+      <LoginContainer>
+        <Logo /> {/* //find logo */}
+        <LoginButton>Sign in with Google</LoginButton>
+      </LoginContainer>
     </Container>
   );
 }
 
 export default Login;
 
-const Container = styled.div``;
+const Container = styled.div`
+display: grid;
+place-items: center;
+height: 100vh;
+background-color: whitesmoke;
+`;
 
-const LoginContainer = styled.div``;
+const LoginContainer = styled.div`
+padding: 200px;
+display: flex;
+flex-direction: column;
+background-color: white;
+border-radius: 51%;
+box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 
-const Logo = styled.div``;
+`;
+
+const Logo = styled.div` //still need to find proper logo
+
+`;
+
+const LoginButton = styled.button`
+color: black;
+`;
