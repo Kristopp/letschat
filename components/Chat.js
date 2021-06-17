58f3af04879b1c51dd4324d0ee6ee18f86763099
Kristo Palu <44571530+Kristopp@users.noreply.github.com>
2021-06-17 10:47:59 +0300

    added users email address behind there avatar

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
+import getRecipentEmail from "../utils/getRecipentEmail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
 
 function Chat({ id, users }) {
+  const [user] = useAuthState(auth);
+  const somethingSomeyhing = getRecipentEmail(users, user);
+  const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipentEmail(users, user)))
+
+  console.log(users, user)
+ /*  console.log(getRecipentEmail(users, user)) */
   return (
     <Container>
       <UserAvatar />
-      recipent email
+      <p>{somethingSomeyhing}</p>
     </Container>
   );
 }
