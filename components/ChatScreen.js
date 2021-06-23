c9ef07c6724c5fba64ec8a2aa934e367d567f560
Kristo Palu <44571530+Kristopp@users.noreply.github.com>
2021-06-23 10:05:28 +0300

    get chat data from firestore using fireBase hooks

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,14 +1,43 @@
 import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import { Avatar, IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp")
+  );
+
+  const showMessage = () => {
+    return;
+  };
+
   return (
     <Container>
       <Header>
         <Avatar />
+        <HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last seen...</p>
+        </HeaderInformation>
+        <IconButton>
+          <AttachFileIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
       </Header>
+      <MessageContainer>{/* Show messages */}</MessageContainer>
     </Container>
   );
 }
@@ -17,6 +46,31 @@ export default ChatScreen;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
 
-const HeaderInfo = styled.div``;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+  //Targeting h3 tag
+  > h3 {
+    margin-bottom: 3px;
+  }
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div``;
